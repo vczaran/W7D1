d69fa00d8f0524b4be8dbd01bd2aa4fdffe1e4b3
Viktoria Czaran <vczaran@gmail.com>
2023-06-12 15:26:38 -0400

    Completed session new form

--- ninety-nine-cats-auth/app/controllers/users_controller.rb
+++ ninety-nine-cats-auth/app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+
   def new
-    
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      redirect_to cats_url
+    else
+      render :new
+    end
+
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- ninety-nine-cats-auth/config/routes.rb
+++ ninety-nine-cats-auth/config/routes.rb
@@ -8,10 +8,16 @@ Rails.application.routes.draw do
   resources :cats, except: :destroy do
     resources :cat_rental_requests, only: [:new]
   end
+
   resources :cat_rental_requests, only: [:new, :create] do
     member do
       post :approve
       post :deny
     end
   end
+
+  resources :users, only: [:new, :create]
+
+  resource :session, only: [:new, :create, :destroy]
+
 end
