632eb0b99b8d104440394bae3a670d3e3825a175
artemplv <artemplv@gmail.com>
2023-06-12 16:23:23 -0400

    implement login / logout partly

--- ninety-nine-cats-auth/app/controllers/sessions_controller.rb
+++ ninety-nine-cats-auth/app/controllers/sessions_controller.rb
@@ -4,5 +4,23 @@ class SessionsController < ApplicationController
         render :new
     end
 
+    def create
+        user = User.find_by_credentials(session_params)
+        session[:session_token] = user.reset_session_token!
+        redirect_to cats_url
+    end
 
+    def destroy
+        if logged_in?
+            @current_user.reset_session_token!
+            session[:session_token] = nil
+        end
+
+        redirect_to new_session_url
+    end
+
+    private
+    def session_params
+        params.require(:session).permit(:username, :password)
+    end
 end
--- ninety-nine-cats-auth/app/models/user.rb
+++ ninety-nine-cats-auth/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     before_validation :ensure_session_token
     validates :username, :password_digest, :session_token, presence: true
     validates :username, :session_token, uniqueness: true
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, allow_nil: true
     attr_reader :password
 
 
@@ -22,6 +22,8 @@ class User < ApplicationRecord
 
     def reset_session_token!
         self.session_token = generate_session_token
+        self.save!
+        self.session_token
     end
 
     def self.find_by_credentials(username, password)
